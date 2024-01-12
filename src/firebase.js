f2f51631bd5ba1a2aac081eda5fe448a2ae461e2
Madhur Pathak <madhurpathak000@gmail.com>
2024-01-12 15:44:44 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCg4LAli-L_qtJv8Y2rr3hk5hrXzql9B7c",
-  authDomain: "chatflow-fe46f.firebaseapp.com",
-  projectId: "chatflow-fe46f",
-  storageBucket: "chatflow-fe46f.appspot.com",
-  messagingSenderId: "1047422468925",
-  appId: "1:1047422468925:web:43c1bcdd1af2bd46c3fb54",
-  measurementId: "G-F74CTBK02R",
+  apiKey: "AIzaSyD1aKGccSs9wZcJdulkMRX58UandXph5Sk",
+  authDomain: "chatfloww-d6e01.firebaseapp.com",
+  projectId: "chatfloww-d6e01",
+  storageBucket: "chatfloww-d6e01.appspot.com",
+  messagingSenderId: "554823743388",
+  appId: "1:554823743388:web:d7195cf5c9862b1facba59"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,41 +1,22 @@
 import React, { useState } from "react";
 import "../style.scss";
 import Add from "../img/addImg.png";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, storage } from "../firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Register = () => {
-  const [err, setErr] = useState(false);
+  const [err, setArr] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const userName = e.target[0].value;
+    const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
-
-      const storageRef = ref(storage, userName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        (error) => {
-          setErr(true);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateProfile(res.user, {
-              userName,
-              photoURL: downloadURL,
-            });
-          });
-        }
-      );
     } catch (err) {
-      setErr(true);
+      setArr(true);
     }
   };
 
@@ -58,7 +39,6 @@ const Register = () => {
             <span>Add an Avatar</span>
           </label>
           <button>Sign Up</button>
-          {err && <span>Somthing went Wrong...</span>}
         </form>
         <p>You do have an Account? Login</p>
       </div>
